e0c6d1156320d1e5ac3ba41fcdb66eea670d0edc
davidmp <mamanipari@gmail.com>
2019-03-27 09:35:26 -0500

    Primer ejemplo en Java

---
+++ AlgoritmosGrupoA/src/AlgoritmosGrupoA.java
@@ -0,0 +1,51 @@
+
+import java.util.Scanner;
+
+public class AlgoritmosGrupoA {
+
+    public static void main(String[] args) {
+        // Definir Variables segun tipo de datos
+        int ti=0;//Inicializando Valores
+        String di,tu;
+        double st,imp, tp;
+        //Leer Valores desde el Teclado
+        Scanner leer=new Scanner(System.in);
+        System.out.println("Ingrese la cantidad de Minutos:");
+        ti=leer.nextInt();        
+        System.out.println("Ingrese el día de la llamada:");
+        di=leer.next();        
+        System.out.println("Ingrese el Turno de llamada:");
+        tu=leer.next();
+        
+        if(ti<=5){
+            st=ti*1;
+        }else if(ti<=8){
+            st=(ti-5)*0.80+5;
+        }else if(ti<=10){
+            st=(ti-8)*0.70+7.4;
+        }else{
+            st=(ti-10)*0.50+8.8;
+        }
+        
+        if(di.equals("Domingo")){//equals es para comparar cadena
+            imp=st*0.03;
+        }else{
+            if(tu.equals("Matutino")){
+                imp=st*0.15;    
+            }else{
+                imp=st*0.10;
+            }
+        }
+        tp=st+imp;
+        //Imprimir datos de Salida
+        System.out.println("El subtotal de la llamada es:"+st);
+        System.out.println("El Impuesto a pagar es:"+imp);
+        System.out.println("Total a pagar es:"+tp);
+        System.out.println("La llamada se realizó en el día: "+di);
+        System.out.println("La llamada fue en Turno: "+tu);
+        
+        
+        
+    }
+    
+}
