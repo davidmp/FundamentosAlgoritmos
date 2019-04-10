20535b80a3cd59649d16325f87e6f1ab5aa66623
davidmp <mamanipari@gmail.com>
2019-04-10 12:09:52 -0500

    Ejemplo en python GrupoB Cambio 1

--- AlgoritmosGrupoBPy/AlgoritmosGrupoBPy.py
+++ AlgoritmosGrupoBPy/AlgoritmosGrupoBPy.py
@@ -8,4 +8,24 @@ def sumaCantidades():
         cont=cont+1
     print("La suma de los numeros ingresados es:",sumt)
 
-sumaCantidades()
+def tablamultiplicar1a10():
+    for numI in range(1,11):
+        for numF in  range(1,11):
+            print(numI,"x", numF, "=", numI*numF)
+        print("\n")
+
+def main(opcion):
+    switcher={
+        1: tablamultiplicar1a10(),
+        2: sumaCantidades()
+    }
+    func=switcher.get(opcion,"no existe")
+    return func
+
+opcion=int(input("Ingrese el numero del algoritmo que desea probar :"))
+while(opcion!=0):    
+    if opcion==1:
+        tablamultiplicar1a10()
+    else:
+        sumaCantidades()    
+    opcion=int(input("Ingrese el numero del algoritmo que desea probar :"))
