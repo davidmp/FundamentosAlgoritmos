32fdeb785e3c079975d16daba9758c5737ebcec4
davidmp <mamanipari@gmail.com>
2019-04-22 10:20:17 -0500

    Ejemplo de Subprograma Parte Grande Numeros

--- AlgoritmosGrupoB/src/pe/edu/upeu/SubPrograma.java
+++ AlgoritmosGrupoB/src/pe/edu/upeu/SubPrograma.java
@@ -5,6 +5,7 @@
  */
 package pe.edu.upeu;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 /**
@@ -23,10 +24,34 @@ public class SubPrograma {
         }
     return result;
     }
+    long factorialLong(int numero){
+    long contador=1;
+    long result=1;//definir variable local
+        while (contador<=numero) {            
+          result=result*contador;
+          contador++;//Incrementa en una unidad
+        }
+    return result;
+    }
+    BigInteger factorialBigNumber(int numero){
+    long contador=1;
+    BigInteger result=new BigInteger("1");//definir variable local
+        while (contador<=numero) {            
+          result=result.multiply(BigInteger.valueOf(contador));
+          contador++;//Incrementa en una unidad
+        }
+    return result;
+    }
     
-    void calcFactRangoNum(int numInicio, int numFinal){      
+    void calcFactRangoNum(int numInicio, int numFinal){         
         for (int i = numInicio; i <= numFinal; i++) {
+            if(i>=65){
+            System.out.println("El Factorial de "+i+" es:"+factorialBigNumber(i));
+            }else if(i>=34){
+            System.out.println("El Factorial de "+i+" es:"+factorialLong(i));
+            }else{
             System.out.println("El Factorial de "+i+" es:"+factorial(i));
+            }
         }
     }
     
