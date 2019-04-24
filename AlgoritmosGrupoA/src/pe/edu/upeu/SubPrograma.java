2967e19e2f25013ac7d6ced39a4b85e0d20068fa
davidmp <mamanipari@gmail.com>
2019-04-24 09:28:35 -0500

    Sobre carga de metodos Grupo A

--- AlgoritmosGrupoA/src/pe/edu/upeu/SubPrograma.java
+++ AlgoritmosGrupoA/src/pe/edu/upeu/SubPrograma.java
@@ -5,6 +5,9 @@
  */
 package pe.edu.upeu;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.math.BigInteger;
 import java.util.Scanner;
 
@@ -56,15 +59,21 @@ public class SubPrograma {
         }  
     }
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Ingrese un Rango de Numeros");
         System.out.print("Ingrese el Primer numero:");
         int numI=leer.nextInt();
         System.out.print("\nIngrese el numero Final:");
-        int numF=leer.nextInt();
-        
+        int numF=leer.nextInt();        
         SubPrograma sp=new SubPrograma();//sp esun Objeto de la Clase SubPrograma
-        sp.calcFactRangoNum(numI, numF);
+        sp.calcFactRangoNum(numI, numF);                
+        
+        BufferedReader br = new BufferedReader(new 
+        InputStreamReader(System.in)); //Ya tenemos el "lector"        
+        System.out.println("Ingrese el nombre:");
+        String nombre=br.readLine();
+        System.out.println("El nombre es:"+nombre);
+                
     }
    
 }
