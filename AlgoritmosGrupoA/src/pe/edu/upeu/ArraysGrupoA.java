b6236043d6c19554a881ef22a7d9dc35c59a3e98
davidmp <mamanipari@gmail.com>
2019-05-27 12:57:10 -0500

    Matrices Grupo A

--- AlgoritmosGrupoA/src/pe/edu/upeu/ArraysGrupoA.java
+++ AlgoritmosGrupoA/src/pe/edu/upeu/ArraysGrupoA.java
@@ -4,7 +4,6 @@
  * and open the template in the editor.
  */
 package pe.edu.upeu;
-
 /**
  *
  * @author Docente
@@ -17,8 +16,7 @@ public class ArraysGrupoA {
            tablaM[i][1]=(i+1)*(i+1);           
         }
         return tablaM;
-    }
-    
+    }    
     public void sumaDeValoresxParesImpar(){
         int[][]  datos={{3,40,70,30},{50,5,20,4},{60,18,6,10},{16,5,9,7}};
         int par=0, impar=0, todos=0;        
@@ -37,6 +35,47 @@ public class ArraysGrupoA {
         System.out.println("todos:"+todos);
      }
     
+    public int[][] transformada01(int dimensionX, int numInit){
+        int[][] matriz=new int[dimensionX][dimensionX];
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                if(j<dimensionX-i){
+                    matriz[i][j]=numInit+(i+j)*(i+j+1)/2+i;
+                }else{
+                matriz[i][j]=-1;
+                }
+            }
+        }        
+    return matriz;
+    }
+    public int[][] transformada05(int dimensionX, int numInit){
+        int[][] matriz=new int[dimensionX][dimensionX];
+        int contador=0;
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = matriz[0].length-1; j>=0; j--) {
+                if(contador<=i){
+                    matriz[i][j]=numInit;
+                    numInit++;
+                    contador++;
+                }else{
+                matriz[i][j]=-1;
+                }
+            } contador=0;
+        }        
+    return matriz;
+    }
+    
+    
+    
+    public void imprimirMatriz(int[][] matriz){
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                System.out.print(matriz[i][j]+"\t");
+            }
+            System.out.println("");
+        }
+    }
+    
     public static void main(String[] args) {
         ArraysGrupoA ag=new ArraysGrupoA();
         int[][] matriX=ag.cuadradosXnum(10);
@@ -45,6 +84,10 @@ public class ArraysGrupoA {
         }
         System.out.println("Resultados de Suma de contenido de Matriz");
         ag.sumaDeValoresxParesImpar();
+        System.out.println("");
+        ag.imprimirMatriz(ag.transformada01(20, 5));
+        System.out.println("");
+        ag.imprimirMatriz(ag.transformada05(20, 0));
         
     }
 }
